eef12eb7c1ee561ad690389669b52bb6d1c4b7ee
Jonathan Bernal <bernaljonathan179@gmail.com>
2023-08-27 21:51:04 -0600

    test

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+=tqd!d!^d1@+^dmosrrd+@mbp5**ui0mhcb(5=vuu8f!_)7su'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
